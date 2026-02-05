54778028b0b1b9f374cb03b282fa1b326453992f
Yinghao Jin <yinghao.jin@intel.com>
2026-02-05 11:30:42 +0800

    Update bench.sh

--- bench.sh
+++ bench.sh
@@ -3,6 +3,7 @@
 ip_addr=127.0.0.1
 port=30002
 model_path=/data/Qwen3-30B-A3B
+model_name=$(basename $model_path)
 
 test_benchmark_serving_range() {
     local_input=$1
@@ -15,7 +16,7 @@ test_benchmark_serving_range() {
     local_request_rate=inf
     echo "running benchmark serving range test, input len: $local_input, output len: $local_output, len ratio: $local_len_ratio, concurrency: $local_max_concurrency"
 
-    log_name_prefix=benchmark_serving-sla_${model_path}_cardnumber_${local_tp_size}_datatype_${dtype}_in_${local_input}_out_${local_output}_ratio_${local_ratio}_rate_${local_request_rate}_prompts_${local_num_prompts}_random_concurrency_${local_max_concurrency}
+    log_name_prefix=benchmark_serving-sla_${model_name}_cardnumber_${local_tp_size}_datatype_${dtype}_in_${local_input}_out_${local_output}_ratio_${local_ratio}_rate_${local_request_rate}_prompts_${local_num_prompts}_random_concurrency_${local_max_concurrency}
     log_name=${log_name_prefix}_$(TZ='Asia/Shanghai' date +%F-%H-%M-%S)
     #log_name=benchmark_serving_${model_path}_batchsize_${local_max_concurrency}_in_${local_input}_out_${local_output}_ratio_${local_len_ratio}_rate_inf_prompts_${local_num_prompts}_$(TZ='Asia/Shanghai' date +%F-%H-%M-%S)
 
